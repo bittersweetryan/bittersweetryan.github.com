784adf8d0a8f7c0922cd582fc8e47ea984eab605
Ryan Ryan <ryan.anklam@gmail.com>
2012-10-15 14:42:37 -0500

    added snack.js for ajax

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
-;(function(window, s){
+window.app = (  function( window, s, snack ){
 
-}(window, window.Sizzle));
+	return { 
+		loadData : function( ) {
+			var opts = {
+				method : 'post',
+				url : 'resume.json'
+			};
+
+			var data = snack.request( opts, function( err, response ) {
+				if( err ){
+					return;
+				}
+				else{
+					console.log( response );
+				}
+			} );
+		}
+	};
+
+}( window, window.Sizzle, window.snack ) );
