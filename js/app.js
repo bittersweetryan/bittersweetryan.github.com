ed9bf7f3dfbe4230472e070e96116d1aba8c4743
Ryan Ryan <ryan.anklam@gmail.com>
2012-10-20 09:09:13 -0500

    make responsive

--- js/app.js
+++ js/app.js
@@ -1,50 +1,72 @@
 window.app = (  function( window, $, snack ){
-	"use strict";
+	'use strict';
+
+	var navHeight = 0;
 
 	var parseData = function( data ) {
 
 		var json     = snack.parseJSON( data ),
 			key      = null,
 			ele      = null,
-			val      = null;
+			val      = null,
+			sections = $('[data-content]'),
+			section  = null;
+
+		document.title =  'Resumé for ' + json.name;
+
+		$( '#contact' )[0].innerHTML = json.contact;
 
 		for( key in json ){
 
 			val = json[ key ];
 			ele = $( '[data-content=' + key + ']' )[ 0 ];
 
-			//standard keys just populate the section
-			if( !snack.isArray( val ) ){
+			if( ele ){
+				//standard keys just populate the section
+				if( !snack.isArray( val ) ){
 
-				if( ele ) ele.innerHTML = val;
-			}
-			else if( snack.isArray( val ) ){
-				var frag = '';
+					ele.innerHTML = val;
+				}
+				else if( snack.isArray( val ) ){
+					var frag = '';
 
-				snack.each( val , function( item, index ){
-					var template = $( '#' + key + '-template' )[ 0 ];
+					snack.each( val , function( item, index ){
+						var template = $( '#' + key + '-template' )[ 0 ].innerHTML
+								.replace( /^\s\s*/, '' )
+								.replace( /\s\s*$/, '' );
 
-					//template found, key is a string
-					if( template && typeof item === 'string' ){
+						//template found, key is a string
+						if( template && typeof item === 'string' ){
 
-						frag = frag + template.innerHTML.replace('${' + key + '}', item );
-					}
-					else if( template && typeof item === 'object' ){
-
-						var cur = template.innerHTML;
-
-						for( var innerKey in item ){
-							cur = cur.replace('${' + innerKey + '}', item[ innerKey ] );
+							frag = frag + template.replace('${' + key + '}', item );
 						}
+						else if( template && typeof item === 'object' ){
 
-						frag = frag + cur;
-					}
-				});
+							var cur = template;
 
-				ele.innerHTML = frag;
+							for( var innerKey in item ){
+								cur = cur.replace('${' + innerKey + '}', item[ innerKey ] );
+							}
+
+							if(index === val.length-1){
+								cur = cur.replace('<li>','<li class="last">');
+							}
+
+							frag = frag + cur;
+						}
+					});
+
+					ele.innerHTML = frag;
+				}
 			}
-			else if( typeof val === 'object'){
+		}
 
+		for( var i = 0; i < sections.length; i++ ){
+			section = sections[ i ];
+
+			if( !section.innerHTML.match( /\w/ ) ){
+
+				section.parentNode.style.display = 'none';
 			}
 		}
 	};
