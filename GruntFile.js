4e15421c971438ffbd5442957c7ef8ed0c01e3e4
Ryan Ryan <ryan.anklam@gmail.com>
2013-03-28 21:55:46 -0500

    fixed grunt

--- GruntFile.js
+++ GruntFile.js
@@ -6,18 +6,30 @@ module.exports = function(grunt) {
     uglify: {
       options: {
       },
-      build: {
+      sizzle: {
         files: {
-          'js/jsonme.min.js' : ['js/sizzle.js', 'snack.min.js', 'app.js']
+          'js/sizzle.min.js' : ['js/sizzle.js']
         }
+      },
+      jsonme: {
+        files: {
+          'js/app.min.js' : ['js/app.js']
+        }
+      }
+    },
+    concat : {
+      dist: {
+        src : ['js/sizzle.min.js', 'js/snack.min.js', 'js/app.min.js'],
+        dest : 'js/jsonme.min.js'
       }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'concat']);
 
 };
